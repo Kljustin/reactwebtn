02dd77a4d216643d6f92c4ac680b8e0ff5012e31
name <DH52100180@student.stu.edu.vn>
2025-04-09 00:38:50 +0700

    chinh sua lan 1

--- src/Components/Api/ApiService.js
+++ src/Components/Api/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const connection = axios.create({
-    baseURL : 'http://127.0.0.1:8000',
+    baseURL : 'http://apiwebtnghiem.infy.uk',
     headers :{
         'Content-Type' : 'application/json'
     }
